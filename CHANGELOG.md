323bb0fcadd58c250ef30ce59ce6677143e78295
JoaoGabriel <joaogabrieldev@gmail.com>
2023-01-13 21:24:25 -0300

    First committ

---
+++ CHANGELOG.md
@@ -0,0 +1,33 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [1.0.0] - 2020-08-05
+
+### Added
+
+- A SIGNATURE FRONTEND example with BRy KMS.
+- README
+- CHANGELOG
+- LICENSE
+
+[1.0.0]: https://gitlab.com/brytecnologia-team/integracao/api-assinatura/react/geracao-diploma-kms/-/tags/1.0.0
+
+## [1.1.0] - 2022-04-27
+
+### Added
+
+- Form to copy DadosRegistro from Documentação Acadêmica to Diplomado.
+
+- Options to send the documents to the Backend.
+
+- Option to use the UUID of a certificate.
+
+### Changed
+
+- URL to send the data.
+
+- Values of variables in order to conform with version 1.04 of Diploma.
---
+++ src/pages/KMS/index.js
@@ -0,0 +1,143 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function KMS() {
+
+  // ESTADOS ONDE AS VARIÁVIES E VALORES DOS INPUTS FICAM ARMAZENADAS
+  const [documento, setDocumento] = useState(null);
+  const [tipoAssinante, setTipoAssinante] = useState("Representantes");
+  const [tipoDocumento, setTipoDocumento] = useState("XMLDiplomado")
+  const [signerCPF, setSignerCPF] = useState("");
+  const [uuid, setUuid] = useState("");
+  const [loadingAssinatura, setLoadingAssinatura] = useState(false);
+
+  // FUNÇÃO QUE SERÁ EXECUTADA AO ENVIAR O FORMULÁRIO DE ASSINATURA
+  function handleSubmit(event) {
+    // Evita que a página atualize ao dar um submit
+    event.preventDefault();
+
+    assina();
+  }
+
+  // FUNÇÃO QUE SERÁ CHAMADA PELO FORMULÁRIO PARA ASSINAR O DOCUMENTO
+  async function assina() {
+    setLoadingAssinatura(true);
+
+    const data = new FormData();
+    data.append('documento', documento);
+    data.append('tipoAssinatura', tipoAssinante)
+    data.append('signerKMS', signerCPF);
+    data.append('uuid', uuid);
+
+    try {
+      // REALIZA REQUISIÇÃO PARA O BACKEND
+      const response = await axios.post(`http://localhost:3333/${tipoDocumento}/assinaKms`, data);
+      console.log("response", response);
+      if(response.data.message) {
+        alert(response.data.message)
+      } else {
+        // REALIZA O DOWNLOAD DO DIPLOMA ASSINADO
+        window.location.href = response.data.documentos[0].links[0].href;
+      }
+
+    } catch (err) {
+      // DA UM ALERT COM A MENSAGEM DE ERRO
+      alert(err);
+    }
+    setLoadingAssinatura(false);
+  }
+
+  return (
+    <>
+      <div className="row justify-content-center align-items-center">
+        <div className="col-12">
+
+          <form onSubmit={handleSubmit}>
+            <link
+              rel="stylesheet"
+              href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+            />
+
+            <label htmlFor="docLabel">Documento a ser assinado:</label>
+            <label htmlFor="documento" className="fileUp">
+              {documento ? (
+                <React.Fragment>{documento.name}</React.Fragment>
+              ) : (
+                  <React.Fragment>
+                    <i className="fa fa-upload"></i>
+                  Selecione o arquivo
+                  </React.Fragment>
+                )}
+              <input
+                id="documento"
+                type="file"
+                onChange={event => setDocumento(event.target.files[0])}
+              />
+            </label>
+
+            <label htmlFor="tipoDocumento">
+              Tipo de Documento: *
+            </label>
+            <select
+              id="tipoDocumento"
+              value={tipoDocumento}
+              required
+              onChange={event => { setTipoAssinante("Representantes") ; setTipoDocumento(event.target.value)}}
+            >
+              <option value="XMLDiplomado">XML Diplomado</option>
+              <option value="XMLDocumentacaoAcademica">XML Documentação Acadêmica</option>
+            </select>
+
+            <label htmlFor="tipoAssinante">
+              Tipo de assinante: *{" "}
+            </label>
+            <select
+              id="tipoAssinante"
+              value={tipoAssinante}
+              required
+              onChange={event => setTipoAssinante(event.target.value)}
+            >
+              <option value="Representantes">Representantes:</option>
+              {tipoDocumento === "XMLDiplomado" ? 
+                (
+                  <>
+                    <option value="IESRegistradora">IES Registradora</option>            
+                  </> 
+                ) : 
+                (
+                  <>
+                    <option value="IESEmissoraDadosDiploma">IES Emissora em Dados Diploma</option>   
+                    <option value="IESEmissoraRegistro">IES Emissora para registro</option>           
+                  </> 
+                )
+              }
+            </select>
+
+            <label htmlFor="signerCPF">CPF do assinante:</label>
+            <input
+              type="number"
+              id="CPF do assinante"
+              placeholder="CPF do assinante"
+              value={signerCPF}
+              onChange={event => setSignerCPF(event.target.value)}
+            />
+
+            <label htmlFor="uuid">UUID do certificado:</label>
+            <input
+              type="string"
+              id="uuid"
+              placeholder="UUID"
+              value={uuid}
+              onChange={event => setUuid(event.target.value)}              
+            />
+            <button className="btn" type="submit">
+              Assinar
+            </button>
+
+            <div>{loadingAssinatura ? "Realizando a assinatura do documento..." : ""}</div>
+          </form>        
+        </div>
+      </div>
+    </>
+  );
+}
